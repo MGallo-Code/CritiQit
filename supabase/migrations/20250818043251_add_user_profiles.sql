1ead24c3ca3cbc15e66ffdea3886b1c45053dde7
MGallo-Code <mgallo.code@gmail.com>
2025-09-07 14:42:16 -0400

    Update db migration for profile upsert (and additional delete perms)

--- supabase/migrations/20250818043251_add_user_profiles.sql
+++ supabase/migrations/20250818043251_add_user_profiles.sql
@@ -145,24 +145,40 @@ using ((( SELECT auth.uid() AS uid) = id));
 CREATE TRIGGER on_auth_user_created AFTER INSERT ON auth.users FOR EACH ROW EXECUTE FUNCTION handle_new_user();
 
 
-  create policy "Anyone can update their own avatar."
-  on "storage"."objects"
-  as permissive
-  for update
-  to public
-using ((( SELECT auth.uid() AS uid) = owner))
-with check ((bucket_id = 'avatars'::text));
 
+CREATE POLICY "Users can upload an avatar to their own folder."
+ON storage.objects
+as permissive
+FOR insert
+to authenticated
+with check (
+    -- restrict bucket
+    bucket_id = 'avatars'
+    and (select auth.uid()::text) = (storage.foldername(name))[1]
+);
 
+CREATE POLICY "Users can update their own avatars."
+ON storage.objects
+as permissive
+FOR update
+to authenticated
+using (
+    -- restrict bucket
+    (bucket_id = 'avatars'::text) AND (owner = auth.uid())
+)
+with check (
+    (select auth.uid()::text) = (storage.foldername(name))[1]
+);
 
-  create policy "Anyone can upload an avatar."
-  on "storage"."objects"
-  as permissive
-  for insert
-  to public
-with check ((bucket_id = 'avatars'::text));
-
-
+CREATE POLICY "Users can delete their own avatars."
+ON storage.objects
+as permissive
+FOR delete
+to authenticated
+using (
+    -- restrict bucket
+    (bucket_id = 'avatars'::text) AND (owner = auth.uid())
+);
 
   create policy "Avatar images are publicly accessible."
   on "storage"."objects"
