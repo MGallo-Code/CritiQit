f8d89606aec57357fd124774a1f2d992ce327b88
MGallo-Code <mgallo.code@gmail.com>
2025-09-20 01:58:00 -0400

    Add allowed development origins to Next.js configuration

--- apps/critiqit-web/next.config.ts
+++ apps/critiqit-web/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: [
+    "critiqit.io",
+    "api.critiqit.io",
+    "www.critiqit.io",
+  ],
 };
 
 export default nextConfig;
