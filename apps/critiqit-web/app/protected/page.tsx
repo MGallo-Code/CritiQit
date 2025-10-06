e4e5f884cc108322804ff477bbc1c0919a88ed08
MGallo-Code <mgallo.code@gmail.com>
2025-10-06 18:21:30 -0400

    Slightly clean up dashboard page

--- apps/critiqit-web/app/protected/page.tsx
+++ apps/critiqit-web/app/protected/page.tsx
@@ -16,7 +16,7 @@ export default async function ProtectedPage() {
       <div className="w-full">
         <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
+          This is a protected dashboard that you can only see as an authenticated
           user
         </div>
       </div>
@@ -25,9 +25,6 @@ export default async function ProtectedPage() {
         <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
           {JSON.stringify(data.claims, null, 2)}
         </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
       </div>
     </div>
   );
